c5f9f1e209eab93bf0bf8263f49db64b763cef68
Latro78 <77676570+Latro78@users.noreply.github.com>
2021-01-19 15:24:48 +0100

    Delete Convert.py

--- Convert.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-def convertInputArray(array, pixelsCount):
-    arrayConverted = np.where(array <= 0, -1, 1)
-    arrayConverted = arrayConverted.reshape(pixelsCount)
-    return arrayConverted
