d49fc76f8c7a998516393f5924ad6a76a89e4529
Latro78 <77676570+Latro78@users.noreply.github.com>
2021-01-19 18:01:48 +0100

    Add files via upload

--- zad_7/hopfield_nn.py
+++ zad_7/hopfield_nn.py
@@ -1,6 +1,7 @@
 import numpy as np
 from zad_5 import pokaz_bitmap as bm
 from zad_7 import Convert as c
+import time
 
 
 class HopfieldNN:
@@ -24,43 +25,60 @@ class HopfieldNN:
     # uczenie
     def train_image(self, schemes):
         convertedSchemes = np.array(schemes)
-        for s in range(schemes.shape[0]):
-            convertedSchemes[s] = np.where(np.array(schemes) <= 0, -1, 1)
-            convertedSchemes[s] = convertedSchemes[s].reshape(self.n)
+        convertedSchemes = np.where(schemes <= 0, -1, 1)
+
+        # patternsConverted = np.array([ConvertInputArray(np.array(pattern), pixelsCount) for pattern in patterns])
+        convertedSchemesV = np.array([c.convertInputArray(np.array(scheme),self.n) for scheme in convertedSchemes])
         # korekcja wag
+
         for i in range(self.n):
             for j in range(self.n):
                 if i == j:
                     self.weights[i, j] = 0
                 else:
-                    for k in range(convertedSchemes.shape[0]):
-                        self.weights[i, j] += (convertedSchemes[k, i] * convertedSchemes[k, j]) / self.n
+                    for k in range(convertedSchemesV.shape[0]):
+                        self.weights[i, j] += convertedSchemesV[k, i] * convertedSchemesV[k, j] / self.n
+
 
     # korekcja obrazu
     def recognize_image(self, schemes):
+        iterations = 5
         schemeNrRow = self.matrix.shape[0]
         schemeNrCol = self.matrix.shape[1]
         image = np.where(np.array(self.matrix) <= 0, -1, 1)
         image = image.reshape(self.n)
-        imageTmp = np.zeros(self.n)
+        print(image)
+
+
+        imageTmp = np.empty(self.n)
 
         # kroki naprawiajace obraz
         try:
-            for i in range(self.n):
-                for j in range(self.n):
-                    imageTmp[i] += image[i] * self.weights[i, j]
-            imageTmp = np.where(np.array(self.matrix) < 0, 0, 1).reshape(schemeNrRow, schemeNrCol)
-            self.matrix = imageTmp
+            for _ in range(iterations):
+                for i in range(self.n):
+                    imageTmp[i]=0
+                    for j in range(self.n):
+                        imageTmp[i] += image[i] * self.weights[i, j]
+                    time.sleep(1)
+                    print(imageTmp)
+            imageTmp /= self.n
+            imageTmp1 = np.where(imageTmp < 0, -1, 1)
+
+            imageTmp2 = np.where(imageTmp1 == -1, 0, 1).reshape(schemeNrRow, schemeNrCol)
+
+            self.matrix = imageTmp2
 
         except:
             # print jezeli obraz nie zostal zmieniony
            print("Błąd przy korygowaniu obrazu")
 
         finally:
-            return image
+            return self.matrix
+
     # pokazanie bitmapy
     def show_matrix(self):
         bm.show_bm(self.matrix)
+
     # destruktor
     def __del__(self):
         print('Instancja usunięta')
--- zad_7/zad7.py
+++ zad_7/zad7.py
@@ -1,6 +1,17 @@
 import numpy as np
 from zad_7 import schemes as s
 from zad_7 import hopfield_nn
+from zad_5 import pokaz_bitmap as bm
 
 obraz = hopfield_nn.HopfieldNN(5,5)
+obraz.set_matrix(np.array(
+    [[1, 0, 0, 1, 1],
+     [0, 1, 0, 1, 0],
+     [0, 0, 1, 0, 0],
+     [0, 1, 0, 1, 0],
+     [1, 0, 0, 1, 1]]
+))
+obraz.show_matrix()
 obraz.train_image(np.array(s.schemes))
+obrazPoprawiony = obraz.recognize_image(np.array(s.schemes))
+bm.show_bm(obrazPoprawiony)
